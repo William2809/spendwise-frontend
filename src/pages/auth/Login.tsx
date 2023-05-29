09a227d56edb91e2cf50b96370a7397b29711c87
William Huang <william.huang001@binus.ac.id>
2023-05-30 01:40:03 +0900

    add loading screens

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -5,12 +5,14 @@ import { Link, useNavigate } from "react-router-dom";
 import authService from "../../utils/auth/AuthService";
 import useAuthStatus from "../../hooks/useAuthStatus";
 import Google from "../../utils/auth/GoogleAuth";
+import LoadingScreen from "../../components/LoadingScreen";
 export interface LoginForm {
 	email: string;
 	password: string;
 }
 
 function Login() {
+	const [isLoading, setIsLoading] = useState(false);
 	const [visible, setVisible] = useState(true);
 	const [error, setError] = useState(null);
 	const [formData, setFormData] = useState<LoginForm>({
@@ -43,7 +45,9 @@ function Login() {
 		const userData = formData;
 
 		try {
+			setIsLoading(true);
 			await authService.login(userData);
+			setIsLoading(false);
 		} catch (error: any) {
 			setError(error.message);
 		}
@@ -51,80 +55,86 @@ function Login() {
 	};
 
 	return (
-		<div className="flex flex-col justify-center items-center pt-[120px]">
-			<div>
-				<div className="flex items-center gap-4">
-					<img
-						className="w-[80px]"
-						src="/logo.svg"
-						alt=""
-					/>
-					<h1 className="text-primary text-6xl font-bold">Login</h1>
-				</div>
-				<div className="mt-4">
-					<p>
-						Don't have an account?{" "}
-						<Link
-							className="text-primary"
-							to="/register"
-						>
-							Register here
-						</Link>
-					</p>
-				</div>
-			</div>
-			<form
-				onSubmit={onSubmit}
-				className="mt-20 flex flex-col gap-4"
-			>
-				<input
-					type="email"
-					name="email"
-					placeholder="Enter your email"
-					autoComplete="off"
-					className="text-black placeholder:text-disabled bg-input-bg outline-none border-none rounded-lg h-[60px] w-[310px] p-4"
-					value={formData.email}
-					onChange={onChange}
-					required
-				/>
-				<div className="relative flex items-center">
-					<input
-						type={!visible ? "text" : "password"}
-						name="password"
-						placeholder="Enter your Password"
-						autoComplete="off"
-						className=" text-black placeholder:text-disabled bg-input-bg outline-none border-none rounded-lg h-[60px] w-[310px] p-4"
-						value={formData.password}
-						onChange={onChange}
-						required
-					/>
-					<span onClick={togglePassword}>
-						{visible ? (
-							<MdVisibilityOff
-								className=" absolute transform -translate-y-1/2  right-5"
-								size={20}
+		<div>
+			{isLoading ? (
+				<LoadingScreen />
+			) : (
+				<div className="flex flex-col justify-center items-center pt-[120px]">
+					<div>
+						<div className="flex items-center gap-4">
+							<img
+								className="w-[80px]"
+								src="/logo.svg"
+								alt=""
 							/>
-						) : (
-							<MdVisibility
-								className="absolute transform -translate-y-1/2 right-5"
-								size={20}
-							/>
-						)}
-					</span>
-				</div>
-				{error && <p className="text-[#F61C1C]">{error}</p>}
-
-				<div className="mt-10">
-					{/* sign in with google */}
-					<Google.GoogleAuth></Google.GoogleAuth>
-					<button
-						type="submit"
-						className="mt-5 w-[310px] bg-primary text-white rounded-lg font-semibold text-[24px] p-2 hover:bg-primary-hover"
+							<h1 className="text-primary text-6xl font-bold">Login</h1>
+						</div>
+						<div className="mt-4">
+							<p>
+								Don't have an account?{" "}
+								<Link
+									className="text-primary"
+									to="/register"
+								>
+									Register here
+								</Link>
+							</p>
+						</div>
+					</div>
+					<form
+						onSubmit={onSubmit}
+						className="mt-20 flex flex-col gap-4"
 					>
-						Login
-					</button>
+						<input
+							type="email"
+							name="email"
+							placeholder="Enter your email"
+							autoComplete="off"
+							className="text-black placeholder:text-disabled bg-input-bg outline-none border-none rounded-lg h-[60px] w-[310px] p-4"
+							value={formData.email}
+							onChange={onChange}
+							required
+						/>
+						<div className="relative flex items-center">
+							<input
+								type={!visible ? "text" : "password"}
+								name="password"
+								placeholder="Enter your Password"
+								autoComplete="off"
+								className=" text-black placeholder:text-disabled bg-input-bg outline-none border-none rounded-lg h-[60px] w-[310px] p-4"
+								value={formData.password}
+								onChange={onChange}
+								required
+							/>
+							<span onClick={togglePassword}>
+								{visible ? (
+									<MdVisibilityOff
+										className=" absolute transform -translate-y-1/2  right-5"
+										size={20}
+									/>
+								) : (
+									<MdVisibility
+										className="absolute transform -translate-y-1/2 right-5"
+										size={20}
+									/>
+								)}
+							</span>
+						</div>
+						{error && <p className="text-[#F61C1C]">{error}</p>}
+
+						<div className="mt-10">
+							{/* sign in with google */}
+							<Google.GoogleAuth></Google.GoogleAuth>
+							<button
+								type="submit"
+								className="mt-5 w-[310px] bg-primary text-white rounded-lg font-semibold text-[24px] p-2 hover:bg-primary-hover"
+							>
+								Login
+							</button>
+						</div>
+					</form>
 				</div>
-			</form>
+			)}
 		</div>
 	);
 }
--- src/pages/auth/Register.tsx
+++ src/pages/auth/Register.tsx
@@ -4,6 +4,7 @@ import { Link, useNavigate } from "react-router-dom";
 import authService from "../../utils/auth/AuthService";
 import useAuthStatus from "../../hooks/useAuthStatus";
 import Google from "../../utils/auth/GoogleAuth";
+import LoadingScreen from "../../components/LoadingScreen";
 
 export interface RegisterForm {
 	name: string;
@@ -12,6 +13,7 @@ export interface RegisterForm {
 }
 
 function Register() {
+	const [isLoading, setIsLoading] = useState(false);
 	const [visible, setVisible] = useState(true);
 	const [error, setError] = useState(null);
 	const [formData, setFormData] = useState<RegisterForm>({
@@ -46,7 +48,9 @@ function Register() {
 		const userData = formData;
 
 		try {
+			setIsLoading(true);
 			await authService.register(userData);
+			setIsLoading(false);
 		} catch (error: any) {
 			setError(error.message);
 		}
@@ -54,90 +58,96 @@ function Register() {
 	};
 
 	return (
-		<div className="flex flex-col justify-center items-center pt-[100px]">
-			<div>
-				<div className="flex items-center gap-4">
-					<img
-						className="w-[80px]"
-						src="/logo.svg"
-						alt=""
-					/>
-					<h1 className="text-primary text-6xl font-bold">Register</h1>
-				</div>
-				<div className="mt-4">
-					<p>
-						Already have an account?{" "}
-						<Link
-							className="text-primary"
-							to="/"
-						>
-							Login here
-						</Link>
-					</p>
-				</div>
-			</div>
-			<form
-				onSubmit={onSubmit}
-				className="mt-16 flex flex-col gap-4"
-			>
-				<input
-					type="text"
-					name="name"
-					placeholder="Enter your name"
-					autoComplete="off"
-					className="text-black placeholder:text-disabled bg-input-bg outline-none border-none rounded-lg h-[60px] w-[310px] p-4"
-					value={formData.name}
-					onChange={onChange}
-					required
-				/>
-				<input
-					type="email"
-					name="email"
-					placeholder="Enter your email"
-					autoComplete="off"
-					className="text-black placeholder:text-disabled bg-input-bg outline-none border-none rounded-lg h-[60px] w-[310px] p-4"
-					value={formData.email}
-					onChange={onChange}
-					required
-				/>
-				<div className="relative flex items-center">
-					<input
-						type={!visible ? "text" : "password"}
-						name="password"
-						placeholder="Enter your Password"
-						autoComplete="off"
-						className=" text-black placeholder:text-disabled bg-input-bg outline-none border-none rounded-lg h-[60px] w-[310px] p-4"
-						value={formData.password}
-						onChange={onChange}
-						required
-					/>
-					<span onClick={togglePassword}>
-						{visible ? (
-							<MdVisibilityOff
-								className=" absolute transform -translate-y-1/2  right-5"
-								size={20}
+		<div>
+			{isLoading ? (
+				<LoadingScreen />
+			) : (
+				<div className="flex flex-col justify-center items-center pt-[100px]">
+					<div>
+						<div className="flex items-center gap-4">
+							<img
+								className="w-[80px]"
+								src="/logo.svg"
+								alt=""
 							/>
-						) : (
-							<MdVisibility
-								className="absolute transform -translate-y-1/2 right-5"
-								size={20}
-							/>
-						)}
-					</span>
-				</div>
-				{error && <p className="text-[#F61C1C]">{error}</p>}
-
-				<div className="mt-10">
-					{/* sign in with google */}
-					<Google.GoogleAuth></Google.GoogleAuth>
-					<button
-						type="submit"
-						className="mt-5 w-[310px] bg-primary text-white rounded-lg font-semibold text-[24px] p-2 hover:bg-primary-hover"
+							<h1 className="text-primary text-6xl font-bold">Register</h1>
+						</div>
+						<div className="mt-4">
+							<p>
+								Already have an account?{" "}
+								<Link
+									className="text-primary"
+									to="/"
+								>
+									Login here
+								</Link>
+							</p>
+						</div>
+					</div>
+					<form
+						onSubmit={onSubmit}
+						className="mt-16 flex flex-col gap-4"
 					>
-						Create account
-					</button>
+						<input
+							type="text"
+							name="name"
+							placeholder="Enter your name"
+							autoComplete="off"
+							className="text-black placeholder:text-disabled bg-input-bg outline-none border-none rounded-lg h-[60px] w-[310px] p-4"
+							value={formData.name}
+							onChange={onChange}
+							required
+						/>
+						<input
+							type="email"
+							name="email"
+							placeholder="Enter your email"
+							autoComplete="off"
+							className="text-black placeholder:text-disabled bg-input-bg outline-none border-none rounded-lg h-[60px] w-[310px] p-4"
+							value={formData.email}
+							onChange={onChange}
+							required
+						/>
+						<div className="relative flex items-center">
+							<input
+								type={!visible ? "text" : "password"}
+								name="password"
+								placeholder="Enter your Password"
+								autoComplete="off"
+								className=" text-black placeholder:text-disabled bg-input-bg outline-none border-none rounded-lg h-[60px] w-[310px] p-4"
+								value={formData.password}
+								onChange={onChange}
+								required
+							/>
+							<span onClick={togglePassword}>
+								{visible ? (
+									<MdVisibilityOff
+										className=" absolute transform -translate-y-1/2  right-5"
+										size={20}
+									/>
+								) : (
+									<MdVisibility
+										className="absolute transform -translate-y-1/2 right-5"
+										size={20}
+									/>
+								)}
+							</span>
+						</div>
+						{error && <p className="text-[#F61C1C]">{error}</p>}
+
+						<div className="mt-10">
+							{/* sign in with google */}
+							<Google.GoogleAuth></Google.GoogleAuth>
+							<button
+								type="submit"
+								className="mt-5 w-[310px] bg-primary text-white rounded-lg font-semibold text-[24px] p-2 hover:bg-primary-hover"
+							>
+								Create account
+							</button>
+						</div>
+					</form>
 				</div>
-			</form>
+			)}
 		</div>
 	);
 }
