8a62e6299781630c20ce09463fd696749961548e
Hiroto Iwata <hiroto1610@outlook.com>
2023-06-05 17:00:46 +0900

    deals page just jump to the website by button

--- src/pages/Deals.tsx
+++ src/pages/Deals.tsx
@@ -7,16 +7,16 @@ function Deals() {
 
   return (
     <div className="px-5 pt-4">
-      {/*<div className="flex items-center gap-3">
-				<img
-					src={user.picture ? `${user.picture}` : nullpng}
-					className="rounded-full h-10"
-					alt="User Profile"
-				/>
-				<h2 className="text-primary-muted font-semibold text-base">
-					{user.name}
-				</h2>
-			</div>*/}
+      <div className="flex items-center gap-3">
+        <img
+          src={user.picture ? `${user.picture}` : nullpng}
+          className="rounded-full h-10"
+          alt="User Profile"
+        />
+        <h2 className="text-primary-muted font-semibold text-base">
+          {user.name}
+        </h2>
+      </div>
       <div className="App">
         <div className="container mx-auto">
           <Card
