0cb818dd069ec7f58e3f880dca76700e16c2b53b
William Huang <william.huang001@binus.ac.id>
2023-06-17 15:31:39 +0900

    update recommendation layout

--- src/pages/Insights.tsx
+++ src/pages/Insights.tsx
@@ -1,34 +1,74 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import LoadingScreen from "../components/LoadingScreen";
 import insightsService from "../utils/insightsService";
+import nullpng from "../assets/null.png";
 
 function Insights() {
 	const [isLoading, setIsLoading] = useState(false);
+	var data = JSON.parse(localStorage.getItem("user")!);
 	const [recommendation, setRecommendation] = useState(null);
+	const user = JSON.parse(localStorage.getItem("user")!);
 
 	const getRecommendation = async () => {
 		setIsLoading(true);
 		const response = await insightsService.analyzeAndRecommend();
-		setRecommendation(response);
+		// save recommendation to local storage
+		let data = JSON.parse(localStorage.getItem("user")!);
+		data.recommendation = response.replace(/\n/g, "<br>");
+		localStorage.setItem("user", JSON.stringify(data));
+		setRecommendation(data.recommendation);
 		setIsLoading(false);
 	};
 
+	useEffect(() => {
+		setRecommendation(data.recommendation);
+		console.log(data.recommendation);
+	}, []);
+
 	return (
-		<div className="p-5 ">
-			{isLoading && <LoadingScreen />}
-			<div>Recommendations:</div>
-			<div className="mt-10">
-				<div
-					onClick={getRecommendation}
-					className="p-4 bg-primary text-white text-[24px] font-semibold hover:bg-primary-hover rounded-xl max-w-min cursor-pointer"
-				>
-					Get Recommendations
+		<div className="px-5 pt-4 pb-[100px] max-w-lg">
+			<div className="flex justify-between items-center w-full">
+				<div className="flex items-center gap-3">
+					<img
+						src={user.picture ? `${user.picture}` : nullpng}
+						className="rounded-full h-10"
+						alt="User Profile"
+					/>
+					<h2 className="text-primary-muted font-semibold text-base">
+						{user.name}
+					</h2>
 				</div>
-				{recommendation ? (
-					<div className="p-3 rounded-xl mt-3 bg-secondary text-black">
-						{recommendation}
+			</div>
+
+			{isLoading && <LoadingScreen />}
+
+			<div className="pt-4">
+				<div className="w-full rounded-xl bg-primary p-5">
+					<div className="text-[24px] font-semibold text-white ">
+						Next week's prediction
 					</div>
-				) : null}
+					<div className="text-secondary">*Chart*</div>
+				</div>
+
+				<div className="mt-5">
+					<div className="text-[24px] font-semibold text-black my-3">
+						Recommendations
+					</div>
+					<div className="w-full">
+						<div
+							onClick={getRecommendation}
+							className="p-4 bg-primary text-white text-[24px] font-semibold hover:bg-primary-hover rounded-xl  cursor-pointer text-center"
+						>
+							Get Recommendations
+						</div>
+						{recommendation ? (
+							<div
+								className="p-3 rounded-xl mt-3 bg-secondary text-black"
+								dangerouslySetInnerHTML={{ __html: recommendation }}
+							></div>
+						) : null}
+					</div>
+				</div>
 			</div>
 		</div>
 	);
